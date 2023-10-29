20519c6176a8b92fb8e86b4a0006621b903d2d09
Davi Coscarelli <48040161+davicoscarelli@users.noreply.github.com>
2023-10-29 01:07:13 -0300

    Change the welcome message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory, jsonify
+import os
 from AudioStoryGenerator import AudioStoryGenerator
 
 app = Flask(__name__)
@@ -13,4 +14,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
